7154a928db83fc92c2f31eb41fb66b75d8fcc0b1
nDavood <DAtamaNguyen@gmail.com>
2022-06-19 23:03:13 -0700

    added dashboard hero and employee list

--- Client/src/app/dashboard/dashboard.component.ts
+++ Client/src/app/dashboard/dashboard.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   title: string = "Dashboard";
+  icon: string = "dashboard";
+  username: string = "Name"; //pass it in from header user
+  lastLoginTime: string = "< time >";
+  lastIPAddress: string = "< location >";
+  randomQuote: string = "“Java is to JavaScript what Car is to Carpet.” – Chris Heilmann";
+  emplName: string = "Zeus Josh";
+  emplCreated: string = "June 19, 2022";
 
   constructor() { }
 
